bf18d8237b803d86bf8ea649d909a7273455695f
Rafal Kwiecien <Rafsters@interia.pl>
2017-10-22 18:24:52 +0200

    Z13 completed

---
+++ src/Z13.java
@@ -0,0 +1,9 @@
+public class Z13 {
+    public static void main(String[] args){
+        String nazwaPlanety = "Ziemia";
+        long powierzchniaPlanety = 510065284; // km2
+        int obwod = 40041; // km
+        double albedoPlanety = 0.39;
+        boolean planetaZamieszkala = true;
+    }
+}
