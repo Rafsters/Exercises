58da9e22645734f8d68baa4c50717616711dfc1a
Rafal Kwiecien <Rafsters@interia.pl>
2017-10-22 21:55:24 +0200

    Z15 completed

---
+++ src/Z15.java
@@ -0,0 +1,28 @@
+public class Z15 {
+    public static void main(String[] args) {
+        // 493L, false, 3.7, 0x3F, 'B', 0xF7FF, 07246, 1.2e5f, '\u0042', -178609
+
+        long a = 493L;
+        boolean b = false;
+        double c = 3.7;
+        int d = 0x3F;
+        char e = 'B';
+        int f = 0xF7FF;
+        int g = 07246; // octal
+        float h = 1.2e5f;
+        char i = '\u0042';
+        int j = -178609;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+
+    }
+}
