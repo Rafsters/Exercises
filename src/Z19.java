e4a7a4bbf41269eb1616ffe1298deaf0bef4a2dd
Rafal Kwiecien <Rafsters@interia.pl>
2017-10-22 22:40:40 +0200

    Z19 completed

---
+++ src/Z19.java
@@ -0,0 +1,10 @@
+public class Z19 {
+    public static void main(String[] args){
+        String[] dniTygodnia = {"Poniedziałek", "Wtorek", "Środa", "Czwartek",
+                "Piątek", "Sobota", "Niedziela"};
+        System.out.println ("Pierwszy dzień tygodnia to: " + dniTygodnia[0]);
+        System.out.println ("Dni wolne od pracy to: " + dniTygodnia[5] + ", " +
+                dniTygodnia[6]);
+        //System.out.println(dniTygodnia[7]);
+    }
+}
