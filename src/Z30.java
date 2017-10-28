1ed1fe4b98938584e90a05f26d9cf9e748fba701
Rafal Kwiecien <Rafsters@interia.pl>
2017-10-28 22:02:19 +0200

    Z30 completed

---
+++ src/Z30.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Z30 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String PESEL = "";
+        while(PESEL.length() != 11){
+            System.out.println("Podaj PESEL");
+            PESEL = in.nextLine();
+        }
+        if(Integer.valueOf(PESEL.charAt(9))%2 == 0){
+            System.out.println("Płeć: Kobieta");
+        } else{
+            System.out.println("Płeć: Mężczyzna");
+        }
+
+    }
+}
