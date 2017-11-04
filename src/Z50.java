3a4733ad1ac196582cc5c50b85e7de6f1e979199
Rafal Kwiecien <Rafsters@interia.pl>
2017-11-04 20:42:04 +0100

    Z50 completed

---
+++ src/Z50.java
@@ -0,0 +1,8 @@
+public class Z50 {
+    public static void main(String[] args) {
+        System.out.print("Ciąg wielkich liter alfabetu łacińskiego: ");
+        for (char litera='A'; litera <= 'Z'; litera++) {
+            System.out.print(litera + " ");
+        }
+    }
+}
