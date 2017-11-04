d7d1730e0a1bedcdaf320d384137a86deee21930
Rafal Kwiecien <Rafsters@interia.pl>
2017-11-04 23:47:16 +0100

    Z53 completed

---
+++ src/Z53.java
@@ -0,0 +1,9 @@
+public class Z53 {
+    public static void main(String[] args) {
+        int value;
+        for (int i = 0; i < 20; i++) {
+            value = (((int)(Math.random()*(-15)))-5);
+            System.out.println(value);
+        }
+    }
+}
